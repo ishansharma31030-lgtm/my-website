077aa092f34afc49b0826ac687c2f5ce884dbefa
ishansharma31030-lgtm <ishansharma31030@gmail.com>
2026-03-11 21:25:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple function to scroll to the contact section
+function scrollToContact() {
+    const contactSection = document.getElementById('contact');
+    contactSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Log a message to the console when the site is ready
+console.log("MEP Services Website Loaded Successfully");
